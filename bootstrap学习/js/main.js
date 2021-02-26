2d92069ff26798dbaa732d3fe36a86c1ad285fcc
yuliuftd <liuyuftd@gmail.com>
2021-02-26 20:06:04 +1100

    animation and memership index added

--- bootstrap学习/js/main.js
+++ bootstrap学习/js/main.js
@@ -1,56 +1,12 @@
-(function ($) {
-
-/*=============================================
-	=    		 Preloader			      =
-=============================================*/
 function preloader() {
-	$('#ctn-preloader').addClass('loaded');
-	$("#loading").fadeOut(500);
-	// Una vez haya terminado el preloader aparezca el scroll
-
-	if ($('#ctn-preloader').hasClass('loaded')) {
-		// Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-		$('#preloader').delay(90000).queue(function () {
-			$(this).remove();
-		});
-	}
+	const parent = document.getElementById('preloader')
+	const child = document.getElementById('ctn-preloader')
+	parent.removeChild(child)
 }
 
-$(window).on('load', function () {
-	preloader();	
-});
-
-
-
-/*=============================================
-	=    		Mobile Menu			      =
-=============================================*/
-//Submenu Dropdown Toggle
-if ($('.menu-area li.dropdown ul').length) {
-	$('.menu-area .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
-
-}
-
-//Mobile Nav Hide Show
-if ($('.mobile-menu').length) {
-
-	var mobileMenuContent = $('.menu-area .main-menu').html();
-	$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
-
-	//Dropdown Button
-	$('.mobile-menu li.dropdown .dropdown-btn').on('click', function () {
-		$(this).toggleClass('open');
-		$(this).prev('ul').slideToggle(500);
-	});
-	//Menu Toggle Btn
-	$('.mobile-nav-toggler').on('click', function () {
-		$('body').addClass('mobile-menu-visible');
-	});
-
-	//Menu Toggle Btn
-	$('.mobile-menu .menu-backdrop,.mobile-menu .close-btn').on('click', function () {
-		$('body').removeClass('mobile-menu-visible');
-	});
-}
-
-})(jQuery);
+setInterval(() => {
+	if(document.getElementById('ctn-preloader'))
+		preloader();
+	else
+		void(0)
+}, 1900);
